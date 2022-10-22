3f53c6be20f3bcce84e1b1b84302e8e8a93e2b86
Parag Saini <bkparag12@gmail.com>
2022-10-22 14:51:50 +0530

    edited the code.

--- statistics.py
+++ statistics.py
@@ -1,3 +1,13 @@
-
+import math
 def calculateStats(numbers):
-  return None
+  if len(numbers) == 0:
+    return {"avg": math.nan, "max": math.nan, "min": math.nan}
+  sum = 0
+  for i in numbers:
+    sum = sum + i
+  sum = sum/len(numbers)
+  dic = {"avg": sum, "max": max(numbers), "min": min(numbers)}
+  return dic
+
+
+
